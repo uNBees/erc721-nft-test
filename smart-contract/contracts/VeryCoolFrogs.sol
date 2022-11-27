97b9c4755873f5eb4eb29a2be518cf391f663a41
unbeesc9m3 <unbeesc9m3@gmail.com>
2022-11-27 14:16:29 -0600

    dev fee change

--- smart-contract/contracts/VeryCoolFrogs.sol
+++ smart-contract/contracts/VeryCoolFrogs.sol
@@ -127,11 +127,11 @@ contract VeryCoolFrogs is ERC721AQueryable, Ownable, ReentrancyGuard {
   }
 
   function withdraw() public onlyOwner nonReentrant {
-    // This will pay HashLips Lab Team 5% of the initial sale.
+    // This will pay Artfully Giving Team 5% of the initial sale.
     // By leaving the following lines as they are you will contribute to the
     // development of tools like this and many others.
     // =============================================================================
-    (bool hs, ) = payable(0x146FB9c3b2C13BA88c6945A759EbFa95127486F4).call{value: address(this).balance * 5 / 100}('');
+    (bool hs, ) = payable(0x8bCD313def6A9B597D22822e04B74E45177731f8).call{value: address(this).balance * 5 / 100}('');
     require(hs);
     // =============================================================================
 
